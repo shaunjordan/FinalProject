ada150be2d65f0faffe09f8e6e047157eba3b622
Shaun Jordan <s1bravo@gmail.com>
2017-04-17 21:03:43 -0500

    button update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+function buttonClick() {
+document.getElementById("btn").addEventListener("click", navigation, true);
+}
 
+function navigation() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+window.onload = function() {
+  buttonClick();
+}
