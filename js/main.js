e36d0e84f6ad1ecd4775d6e8bf836b98a8338e23
Shaun Jordan <s1bravo@gmail.com>
2017-04-18 14:27:36 -0500

    responsive nav and content updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function navigation() {
+/*function navigation() {
   document.getElementById("btn").onclick = function() {
       if (document.getElementById("dropdown").className == "showmenu") {
         document.getElementById("dropdown").className = "";
@@ -12,3 +12,20 @@ function navigation() {
 window.onload = function() {
   navigation();
 }
+*/
+/*function toggleMenu() {
+  document.getElementById("dropdown").classList.toggle("nav-left");
+  document.getElementById("dropdown").classList.toggle("showmenu");
+}*/
+$(document).ready(function () {
+  $("#btn").click(function() {
+    $("#dropdown").toggleClass("nav-left");
+    $("#dropdown").toggleClass("showmenu");
+  });
+
+  var viewport = $(window).width();
+  if (viewport >= 765) {
+    $("#dropdown").removeClass("showmenu").addClass("nav-left");
+  }
+  
+});
