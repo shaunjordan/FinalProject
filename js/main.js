460a426574f15deb9159e2796d10fead7c71d413
Shaun Jordan <s1bravo@gmail.com>
2017-04-18 22:11:20 -0500

    js updates
    add showmenu temporarily to design drop down menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+//jQuery
+$(document).ready(function () {
+  $("#btn").click(function() {
+    //$("#dropdown").toggleClass("nav-left");
+    $("#dropdown").toggleClass("showmenu");
+  });
+
+  var viewport = $(window).width();
+  if (viewport > 765) {
+    $("#dropdown").removeClass("showmenu").addClass("nav-left");
+  }
+
+});
+
+
+
+
+// Below is first attempt code in pure JS for mobile navigation dropdown
+//It's not pretty but I am leaving it here for my own learning purposes
 /*function navigation() {
   document.getElementById("btn").onclick = function() {
       if (document.getElementById("dropdown").className == "showmenu") {
@@ -17,15 +36,3 @@ window.onload = function() {
   document.getElementById("dropdown").classList.toggle("nav-left");
   document.getElementById("dropdown").classList.toggle("showmenu");
 }*/
-$(document).ready(function () {
-  $("#btn").click(function() {
-    $("#dropdown").toggleClass("nav-left");
-    $("#dropdown").toggleClass("showmenu");
-  });
-
-  var viewport = $(window).width();
-  if (viewport >= 765) {
-    $("#dropdown").removeClass("showmenu").addClass("nav-left");
-  }
-  
-});
