ceb2afb516ab2498189735692ac5ddc038647cec
Shaun Jordan <s1bravo@gmail.com>
2017-05-05 08:21:32 -0500

    final commit

--- js/main.js
+++ js/main.js
@@ -1,41 +1,16 @@
 //jQuery
 $(document).ready(function () {
+  //adds active class cover text on window open
   $(".covertext").addClass("activecover");
+  //dropdown menu for mobile navigation
   $("#btn").click(function() {
     //$("#dropdown").toggleClass("nav-left");
     $("#dropdown").toggleClass("showmenu");
   });
 
-  //removes showmenu if screen > 765
+  //removes showmenu class if screen > 765
   var viewport = $(window).width();
   if (viewport > 765) {
     $("#dropdown").removeClass("showmenu");
   }
-
-  /*/cycles through background-images
-  var i = 0;
-  var time = setInterval(changeImg, 6000);
-  function changeImg(){
-   //background images
-   var images = ["./images/welcome.jpg","./images/tab.jpg","./images/expert.jpg", "./images/contact.jpg"];
-  $('.sliding').css('background', 'url("' + images[i] + '")' + 'no-repeat');
-  $('.sliding').css('background-size', 'cover');
-    if(i == images.length -1){
-         i = 0;
-    }
-    else{
-        i++;
-    }
-  } //end of changeImg()
-    //codesnippet modified from
-    //http://stackoverflow.com/questions/5977837/jquery-loop-through-different-backgrounds
-  var count = -1;
-  function addClass(){
-  var covertext = $(".covertext");
-  var coverLength = covertext.length - 1;
-  count < coverLength ? count++ : count = 0;
-  covertext.removeClass("activecover").eq(count).addClass("activecover");
-  }
-  setInterval(addClass, 6000);*/
-
 });
